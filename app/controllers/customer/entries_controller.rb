1e89cf88a2884b1a681d10d99829aaf9f874aad3
kwtuku <76758720+kwtuku@users.noreply.github.com>
2024-09-19 22:04:53 +0900

    プログラム申し込み時に最大参加者数の超過チェックをサービスオブジェクトに抽出

--- app/controllers/customer/entries_controller.rb
+++ app/controllers/customer/entries_controller.rb
@@ -1,16 +1,11 @@
 class Customer::EntriesController < Customer::Base
   def create
     program = Program.published.find(params[:program_id])
-    if max = program.max_number_of_participants
-      if program.entries.where(canceled: false).count < max
-        program.entries.create!(customer: current_customer)
-        flash.notice = "プログラムに申し込みました。"
-      else
-        flash.alert = "プログラムへの申込者数が上限に達しました。"
-      end
-    else
-      program.entries.create!(customer: current_customer)
+    case Customer::EntryAcceptor.new(current_customer).accept(program)
+    when :accepted
       flash.notice = "プログラムに申し込みました。"
+    when :full
+      flash.alert = "プログラムへの申込者数が上限に達しました。"
     end
     redirect_to [:customer, program]
   end
