38d988744bcaf3b5c38331e82385584b7a18c399
kwtuku <76758720+kwtuku@users.noreply.github.com>
2024-10-03 22:26:56 +0900

    顧客がアカウント情報を更新できる

--- app/controllers/customer/accounts_controller.rb
+++ app/controllers/customer/accounts_controller.rb
@@ -2,4 +2,20 @@ class Customer::AccountsController < Customer::Base
   def show
     @customer = current_customer
   end
+
+  def edit
+    @customer_form = Customer::AccountForm.new(current_customer)
+  end
+
+  def update
+    @customer_form = Customer::AccountForm.new(current_customer)
+    @customer_form.assign_attributes(params[:form])
+    if @customer_form.save
+      flash.notice = "アカウント情報を更新しました。"
+      redirect_to :customer_account, status: :see_other
+    else
+      flash.now.alert = "⼊⼒に誤りがあります。"
+      render action: "edit", status: :unprocessable_entity
+    end
+  end
 end
