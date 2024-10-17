5a53e0092812b60767d2029a626771b481624ed5
kwtuku <76758720+kwtuku@users.noreply.github.com>
2024-10-17 21:43:03 +0900

    アカウント更新の確認画面

--- app/controllers/customer/accounts_controller.rb
+++ app/controllers/customer/accounts_controller.rb
@@ -7,6 +7,18 @@ class Customer::AccountsController < Customer::Base
     @customer_form = Customer::AccountForm.new(current_customer)
   end
 
+  # PATCH
+  def confirm
+    @customer_form = Customer::AccountForm.new(current_customer)
+    @customer_form.assign_attributes(params[:form])
+    if @customer_form.valid?
+      render action: "confirm", status: :accepted
+    else
+      flash.now.alert = "⼊⼒に誤りがあります。"
+      render action: "edit", status: :unprocessable_entity
+    end
+  end
+
   def update
     @customer_form = Customer::AccountForm.new(current_customer)
     @customer_form.assign_attributes(params[:form])
